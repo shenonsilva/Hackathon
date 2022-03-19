8e95fd745837959fd9d80df2bef809e3e1065049
Shenon Oliveira e Silva <shenonsilva@gmail.com>
2022-03-19 02:34:03 -0300

    Alteração 4

---
+++ aulacampbras/src/views/Teladois.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <h2 class="text-h5 text-center mb-3 mt-5">Classificação</h2>
+        <v-img 
+        class="img1 mb-4"
+        src="link" alt="imagem do Brasileirão">
+        </v-img>
+        <p>Texto texto. Texto texto!</p>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Teladois'
+}
+</script>
+
+<style scoped>
+.img1 {
+    border-radius: 5px;
+}
+</style>
